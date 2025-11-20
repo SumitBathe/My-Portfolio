862cb957fd396978300fee8cba1497968643be69
sumitmandates <sumitbathe@mandates.in>
2025-11-20 22:22:20 +0530

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",         // IMPORTANT for custom domain
-  build: {
-    outDir: "docs",  // IMPORTANT
-  },
 })
